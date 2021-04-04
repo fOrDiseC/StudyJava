455e577297018c06421851f9ddb696698e5b94d8
fOrDiseC <72049377+fOrDiseC@users.noreply.github.com>
2021-04-04 23:34:23 +0300

    uid generator

---
+++ src/hackerrank/uid.java
@@ -0,0 +1,15 @@
+package hackerrank;
+
+import java.util.UUID;
+
+public class uid {
+    public static void main(String[] args) {
+
+
+        String UID = UUID.randomUUID().toString();
+
+        System.out.println("uuid=" + UID);
+
+
+    }
+}
