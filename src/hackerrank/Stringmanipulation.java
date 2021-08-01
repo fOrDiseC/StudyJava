757af90d1d04782bd3acea11a5a928302d414e49
fOrDiseC <72049377+fOrDiseC@users.noreply.github.com>
2021-08-01 21:23:09 +0300

    add class

--- src/hackerrank/Stringmanipulation.java
+++ src/hackerrank/Stringmanipulation.java
@@ -14,9 +14,20 @@ public class Stringmanipulation {
         String newA =A.substring(0,1).toUpperCase()+A.substring(1);
         String newB =B.substring(0,1).toUpperCase()+B.substring(1);
         System.out.println(newA+" "+newB);
+        String str = "Java";
+    StringBuffer strBuffer = new StringBuffer(str);
+    System.out.println("Емкость: " + strBuffer.capacity()); // 20
+    strBuffer.ensureCapacity(32);
+    System.out.println("Емкость: " + strBuffer.capacity()); // 42
+    System.out.println("Длина: " + strBuffer.length()); // 4
+    StringBuilder strBild =new StringBuilder(str);
+    strBild.insert(20, );
         
 
+
     }
+
+    
 
 
 
