aeeeca52e776a7bbd527a81ae8ff6022bc048a8b
fOrDiseC <72049377+fOrDiseC@users.noreply.github.com>
2021-08-01 23:32:27 +0300

    new changes

---
+++ src/hackerrank/SubstringJava.java
@@ -0,0 +1,23 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class SubstringJava {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        String subStr = S.substring(start,end);
+
+       System.out.println(subStr);
+        
+        
+    
+    }           
+
+
+    
+}
